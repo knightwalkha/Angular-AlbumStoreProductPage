a8f617c5395b03fa020f7fec80f3e8201d239be6
knightwalkha <ohiosumuankehinde@gmail.com>
2019-11-02 12:31:11 +0100

    update product.service.ts

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -19,7 +19,7 @@ export class ProductService {
     return this._http.get(this._albumUrl).map((response) => <Album>response.json());
   };
 
-  getProducts(_productsUrl): Observable<Product[]> {
+  getProducts(): Observable<Product[]> {
     return this._http.get(this._productsUrl).map(response =>  <Product[]>response.json())
   };
 
